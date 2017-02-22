4c4c071e6e68f4d0f0d6fd3b6613df1ee10a4706
Anthony Castonzo <acastonzo@whalerockindustries.com>
2017-02-22 10:12:56 -0800

    Added newest version of game as well as HTML tutorial page and my page

---
+++ Python/game.py
@@ -0,0 +1,18 @@
+class charClass:
+    HP = 0
+    MP = 0
+    strength = 0
+    speed = 0
+    magic = 0
+
+knight = charClass()
+knight.HP = 300
+knight.MP = 0
+knight.strength = 50
+knight.speed = 4
+knight.magic = 0
+
+mage = charClass()
+mage.HP = 200
+mage.MP = 100
+mage.strength = 10
